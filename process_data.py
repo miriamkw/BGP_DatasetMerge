69030289e87d9d1235ccc979d4511887a69224c3
Miriam K. Wolff <miriamkwolff@hotmail.com>
2024-11-03 17:38:35 +0100

    Worked on notebooks for analysis

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@
 import subprocess
 import shutil
 import os
+import pandas as pd
 
 UNPROCESSED_DATA_PATH = 'unprocessed_data'
 
@@ -31,6 +32,7 @@ def reorganize_results():
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
+    # Move results from data subfolder to a folder named "processed_data"
     for filename in os.listdir(source_folder):
         source_file = os.path.join(source_folder, filename)
         destination_file = os.path.join(destination_folder, filename)
@@ -38,12 +40,47 @@ def reorganize_results():
 
     shutil.rmtree('data')
 
+    # Split files into a folder where each subject has its own file
+    for filename in os.listdir(destination_folder):
+        if ".csv" in filename:
+            filename_without_end = filename.split(".")[0]
+            new_folder_train = os.path.join(destination_folder, filename_without_end, 'train')
+            new_folder_test = os.path.join(destination_folder, filename_without_end, 'test')
+
+            # Create a folder with the name of the file
+            if not os.path.exists(new_folder_train):
+                os.makedirs(new_folder_train)
+                os.makedirs(new_folder_test)
+
+            # Open the df, split into each subject and train / test
+            data_path = os.path.join(destination_folder, filename)
+            df = pd.read_csv(data_path, low_memory=False)
+            subject_ids = df['id'].unique()
+            for subject_id in subject_ids:
+                subset_df = df[df['id'] == subject_id]
+
+                train_df = subset_df[~subset_df['is_test']]
+                test_df = subset_df[subset_df['is_test']]
+
+                subject_data_train_path = os.path.join(new_folder_train, str(subject_id) + '.csv')
+                subject_data_test_path = os.path.join(new_folder_test, str(subject_id) + '.csv')
+
+                drop_cols = ['id', 'is_test']
+                train_df.drop(columns=drop_cols).to_csv(subject_data_train_path, index=False)
+                test_df.drop(columns=drop_cols).to_csv(subject_data_test_path, index=False)
+
+            # Delete original file
+            os.remove(data_path)
+
 
 def main():
     setup_directories()
 
     # COMMENT OUT DATASET HERE IF YOU DON'T WANT TO PROCESS IT
-    parse_dataset("ohio_t1dm", UNPROCESSED_DATA_PATH)
+    #parse_dataset("ohio_t1dm", UNPROCESSED_DATA_PATH)
+    #parse_dataset("tidepool_dataset", UNPROCESSED_DATA_PATH)
+    #parse_dataset("t1dexi", UNPROCESSED_DATA_PATH)
+    parse_dataset("open_aps", os.path.join(UNPROCESSED_DATA_PATH, 'OpenAPS Data/'))
 
     reorganize_results()
 
